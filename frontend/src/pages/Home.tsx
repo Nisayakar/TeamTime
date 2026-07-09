57b3a2978e6fcaed1d2d8a4a79890e243360f68f
Nisayakar <lnisa.yakarl@gmail.com>
2026-07-09 13:58:00 +0300

    Create initial React project structure

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,40 @@
+function Home(){
+    return(
+        <div>
+            <section>
+                <h1>TEAMTIME</h1>
+                <p>
+                    Proje süreçlerini tek platformda yönetin.
+                </p>
+
+            <button>
+                Proje Oluştur
+            </button>
+            
+            <button>
+                Giriş Yap
+            </button>
+           </section>
+        
+             <section>
+            <h2>Nasıl Çalışır?</h2>
+            <br></br>
+            <p>📁 Proje oluştur</p>
+            <p>👥 Takımını davet et</p>
+            <p>📋 Görevleri dağıt</p>
+            <p>📈 İlerlemeyi takip et</p>
+            </section>
+
+            <section>
+                <h2>Neden TeamTime?</h2>
+                <p>Basit</p>
+                <p>Hızlı</p>
+                <p>Takım Odaklı</p>
+                <p>Ücretsiz</p>
+            </section>
+
+        </div>
+    )
+}
+
+export default Home;
